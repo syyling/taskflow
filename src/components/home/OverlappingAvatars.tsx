576878c3da0e7f1d4dbec9d7c4a06a923612dff3
syyling <sy0hiu@gmail.com>
2025-02-01 01:02:47 +0900

    fix: OverlappingAvatars 컴포넌트 props 추가

--- src/components/home/OverlappingAvatars.tsx
+++ src/components/home/OverlappingAvatars.tsx
@@ -1,28 +1,38 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
-export default function OverlappingAvatars() {
-    const avatars = [
-        { name: "A", src: "" },
-        { name: "B", src: "" },
-        { name: "C", src: "" },
-    ];
+export default function OverlappingAvatars({ users }) {
 
     return (
         <div className="flex items-center">
             <div className="flex -space-x-3">
-                {avatars.map((avatar, index) => (
-                    <Avatar
-                        key={index}
-                        className="w-7 h-7 border-2 border-white shadow-md"
-                    >
-                        <AvatarImage src={avatar.src} />
-                        <AvatarFallback className="bg-gray-200 text-gray-500">
-                            {avatar.name}
-                        </AvatarFallback>
-                    </Avatar>
+                {users.map((user, index) => (
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Avatar
+                                    key={index}
+                                    className="w-7 h-7 border-2 border-white shadow-md"
+                                >
+                                    <AvatarImage src={user.user.img}/>
+                                    <AvatarFallback className="bg-gray-200 text-gray-500 text-xs">
+                                        {user.user.name ? user.user.name.charAt(0) : "?"}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>{user.user.name}</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
                 ))}
             </div>
-            <span className="text-gray-500 text-sm ml-3">+2명</span>
+            {/*<span className="text-gray-500 text-sm ml-3">+2명</span>*/}
         </div>
     );
 }
