116f579bd12c861514dcf7662c9ca210d8534b4d
syyling <sy0hiu@gmail.com>
2025-02-01 01:03:36 +0900

    fix: ProjectCard props 추가

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -8,26 +8,54 @@ import { Progress } from "../ui/progress.tsx";
 import OverlappingAvatars from "@/components/home/OverlappingAvatars.tsx";
 import { Calendar } from "lucide-react";
 
-export default function ProjectCard() {
-  return (
-    <Card className="w-full">
-        <CardHeader className="flex flex-col text-left justify-start gap-1">
-            <CardTitle>프로젝트 1</CardTitle>
-            <CardDescription>진행중 마감일 D-7</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col content-start text-left gap-1">
-          <CardDescription>프로젝트 설명이 여기에 들어갑니다</CardDescription>
-        </CardContent>
-        <CardContent className="flex flex-row content-start text-left gap-1">
-            <OverlappingAvatars />
-        </CardContent>
-      <CardFooter className="flex">
-          <div className="flex flex-row items-center w-[100px]">
-              <Calendar className="w-4 h-4 text-gray-500" />
-              <CardDescription className="pl-0.5">12월 31일</CardDescription>
-          </div>
-          <Progress></Progress>
-      </CardFooter>
-    </Card>
-  );
+interface User {
+    id: number;
+    img: string;
+    name: string;
+}
+
+interface Project {
+    name: string;
+    description: string;
+    deadline: Date;
+    progress: string;
+    users?: User;
+}
+
+interface ProjectCardProps {
+    project: Project;
+}
+
+export default function ProjectCard({ project }: ProjectCardProps) {
+
+    function calculateDDay(targetDate: Date): number {
+        const currentDate: Date = new Date();
+
+        const timeDifference: number = targetDate.getTime() - currentDate.getTime();
+        const dayDifference: number = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+        return dayDifference;
+    }
+
+    return (
+        <Card className="w-full">
+            <CardHeader className="flex flex-col text-left justify-start gap-1">
+                <CardTitle>{project.name}</CardTitle>
+                <CardDescription>{project.progress + " 마감일 D-" +  calculateDDay(new Date(project.deadline))}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col content-start text-left gap-1">
+              <CardDescription>{project.description}</CardDescription>
+            </CardContent>
+            <CardContent className="flex flex-row content-start text-left gap-1">
+                <OverlappingAvatars users={project.users} />
+            </CardContent>
+          <CardFooter className="flex">
+              <div className="flex flex-row items-center w-[120px]">
+                  <Calendar className="w-4 h-4 text-gray-500" />
+                  <CardDescription className="pl-0.5">{project.deadline.toString()}</CardDescription>
+              </div>
+              <Progress></Progress>
+          </CardFooter>
+        </Card>
+    );
 }
