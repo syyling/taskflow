c783587f91bb9e0ee4aa2f64378345792b698d4e
syyling <sy0hiu@gmail.com>
2025-02-02 23:02:59 +0900

    feat: ProjectCard에 드롭다운메뉴 추가

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -7,13 +7,39 @@ import { Card, CardContent,
 import { Progress } from "../ui/progress.tsx";
 import OverlappingAvatars from "@/components/home/OverlappingAvatars.tsx";
 import { Calendar } from "lucide-react";
+import { Ellipsis } from 'lucide-react';
 import {Project} from "@/app/Dashboard.tsx";
+import { format } from "date-fns";
+import {useEffect, useState} from "react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Button} from "@/components/ui/button.tsx";
+
+
 
 interface ProjectCardProps {
     project: Project;
 }
 
 export default function ProjectCard({ project }: ProjectCardProps) {
+
+    const [progress, setProgress] = useState(0)
+
+    // Todo: project.startDate 추가 후 로직 수정
+    useEffect(() => {
+        const currentDate: Date = new Date();
+
+        const timeDifference: number = new Date(project.deadline).getTime() - currentDate.getTime();
+        const dayDifference: number = Math.ceil(timeDifference / (1000 * 3600 * 24));
+        const timer = setTimeout(() => setProgress(dayDifference), 500)
+        return () => clearTimeout(timer)
+    }, []);
 
     function calculateDDay(targetDate: Date): number {
         const currentDate: Date = new Date();
@@ -27,8 +53,20 @@ export default function ProjectCard({ project }: ProjectCardProps) {
     return (
         <Card className="w-full">
             <CardHeader className="flex flex-col text-left justify-start gap-1">
-                <CardTitle>{project.name}</CardTitle>
+                <div className="flex justify-between w-full">
+                    <CardTitle className="w-[auto]">{project.name}</CardTitle>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild className="w-[16px] h-[16px]">
+                            <Button variant="ghost" ><Ellipsis /></Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>수정</DropdownMenuItem>
+                            <DropdownMenuItem>삭제</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
                 <CardDescription>{project.progress + " 마감일 D-" +  calculateDDay(new Date(project.deadline))}</CardDescription>
+
             </CardHeader>
             <CardContent className="flex flex-col content-start text-left gap-1">
               <CardDescription>{project.description}</CardDescription>
@@ -39,9 +77,9 @@ export default function ProjectCard({ project }: ProjectCardProps) {
           <CardFooter className="flex">
               <div className="flex flex-row items-center w-[120px]">
                   <Calendar className="w-4 h-4 text-gray-500" />
-                  <CardDescription className="pl-0.5">{project.deadline.toString()}</CardDescription>
+                  <CardDescription className="pl-0.5">{project.deadline ? format(project.deadline, "yyyy-MM-dd") : undefined}</CardDescription>
               </div>
-              <Progress></Progress>
+              <Progress value={progress}></Progress>
           </CardFooter>
         </Card>
     );
