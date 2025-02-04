cbe78e4b24757a6527cfbcdbe8861d83b0a12671
syyling <sy0hiu@gmail.com>
2025-02-04 23:16:33 +0900

    feat: 대시보드 store 생성

---
+++ src/store/useDashBoardStore.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand'
+
+const useDashBoardStore = create((set) => ({
+  checked: false,
+
+  setChecked: () => set((state) => ({ checked: !state.checked })),
+}))
+
+export default useDashBoardStore;
