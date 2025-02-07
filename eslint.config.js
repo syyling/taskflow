ba306caf94d393171095cb6ad60df2660ed2256b
syyling <sy0hiu@gmail.com>
2025-02-08 01:10:01 +0900

    fix: config 수정

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn"
+      "@typescript-eslint/no-unused-vars": "off"
     },
   },
 )
