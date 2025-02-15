179d69b7d31231f641637a2ab33cac9326f8c1f5
syyling <sy0hiu@gmail.com>
2025-02-15 13:22:51 +0900

    feat: enum 파일 생성

---
+++ src/types/enums.tsx
@@ -0,0 +1,14 @@
+export enum AuthLevel {
+  OWNER = "소유자",
+  ADMIN = "관리자",
+  MEMBER = "멤버",
+  VIEWER = "조회자",
+}
+
+export enum Role {
+  FRONTEND = "프론트엔드",
+  BACKEND = "백엔드",
+  PLANNER = "기획자",
+  PM = "PM",
+  DESIGNER = "디자이너",
+}
