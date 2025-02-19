b98f3d93b23626d93e977a42f8ef4bd296523b59
seoyoung <102759992+syyling@users.noreply.github.com>
2025-02-19 12:57:31 +0900

    feat: zustand store 타입 추가

--- src/store/useDashBoardStore.tsx
+++ src/store/useDashBoardStore.tsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
+import { DashboardState } from "@/types/stores.tsx";
 
-
-const useDashBoardStore = create((set) => ({
+const useDashBoardStore = create<DashboardState>((set) => ({
   checked: false,
 
   setChecked: () => set((state) => ({ checked: !state.checked })),
