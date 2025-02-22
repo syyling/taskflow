59075c0a62b1dfe20c42547c4c7c328c31f952ea
syyling <sy0hiu@gmail.com>
2025-02-22 23:40:24 +0900

    feat: ProgressCard 진행상황 계산 로직 추가

--- src/components/detail/ProgressCard.tsx
+++ src/components/detail/ProgressCard.tsx
@@ -1,14 +1,23 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
+import { Card, CardContent } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
+import { useEffect, useState } from 'react';
 
-const ProgressCard = ({ project }) => {
+const ProgressCard = ({ startDate, endDate }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timeDifference: number = endDate.getTime() - startDate.getTime();
+    const dayDifference: number = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    setProgress(dayDifference);
+  }, [startDate, endDate]);
+
   const getProgressClass = (value) => {
     return `h-3 rounded-lg ${
       value < 30
-        ? "[&>div]:bg-red-500"
+        ? '[&>div]:bg-red-500'
         : value < 70
-          ? "[&>div]:bg-yellow-500"
-          : "[&>div]:bg-green-500"
+          ? '[&>div]:bg-yellow-500'
+          : '[&>div]:bg-green-500'
     }`;
   };
 
@@ -18,23 +27,14 @@ const ProgressCard = ({ project }) => {
         <div className="space-y-3">
           <div className="flex justify-between items-center mb-2">
             <div className="flex flex-col">
-              <span className="font-semibold text-lg text-left">
-                전체 진행률
-              </span>
+              <span className="font-semibold text-lg text-left">전체 진행률</span>
               <p className="text-sm text-muted-foreground mt-1 text-left">
-                {project.progress < 30
-                  ? "초기 단계"
-                  : project.progress < 70
-                    ? "진행 중"
-                    : "마무리 단계"}
+                {progress < 30 ? '초기 단계' : progress < 70 ? '진행 중' : '마무리 단계'}
               </p>
             </div>
-            <span className="text-2xl font-bold">{project.progress}%</span>
+            <span className="text-2xl font-bold">{progress}%</span>
           </div>
-          <Progress
-            value={project.progress}
-            className={getProgressClass(project.progress)}
-          />
+          <Progress value={progress} className={getProgressClass(progress)} />
         </div>
       </CardContent>
     </Card>
